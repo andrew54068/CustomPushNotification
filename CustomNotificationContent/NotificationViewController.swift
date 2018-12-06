c3da3da05181c8aa1c65544251bac5e0b466a72e
andrew54068 <andrew0424718012@yahoo.com.tw>
2018-12-07 00:27:42 +0800

    add breakout in notification

--- CustomNotificationContent/NotificationViewController.swift
+++ CustomNotificationContent/NotificationViewController.swift
@@ -9,41 +9,34 @@
 import UIKit
 import UserNotifications
 import UserNotificationsUI
+import SpriteKit
+import GameplayKit
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
-
-    @IBOutlet weak var label: UILabel?
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var button: UIButton!
-    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var gameView: SKView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any required interface initialization here.
+        if let view = gameView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFit
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
     }
     
     func didReceive(_ notification: UNNotification) {
-        guard let payload: [String: AnyObject] = notification.request.content.userInfo as? [String: AnyObject] else { return }
         
-        label?.text = notification.request.content.body
-        
-        let session: URLSession = URLSession(configuration: .default)
-        if let imageUrl: URL = URL(string: payload["url"] as? String ?? "") {
-            let dataTask: URLSessionDataTask = session.dataTask(with: imageUrl) { [weak self] (data, response, error) in
-                guard let self = self else { return }
-                if error == nil, let imageData: Data = data {
-                    DispatchQueue.main.async {
-                        self.imageView.image = UIImage(data: imageData as Data)
-                    }
-                }
-            }
-            dataTask.resume()
-        }
-        
-    }
-    
-    @IBAction func sliderAction(_ sender: UISlider) {
-        label?.text = String(sender.value)
     }
 
 }
